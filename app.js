c370889ac354fa293328626afe120ceca9050661
linlinliu <linlinliu03@gmail.com>
2020-06-27 20:41:54 -0700

    Customize url response

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const request = require("request");
 
-const url = "http://api.weatherstack.com/current?access_key=747ab087e2746709e77999472c1c284c&query=New%20York";
+const url = "http://api.weatherstack.com/current?access_key=747ab087e2746709e77999472c1c284c&query=New%20York&units=f";
 
-request({url:url}, (error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url:url, json:true}, (error,response) => {
+    const temperature = response.body.current.temperature;
+    const feelslike = response.body.current.feelslike;
+    const overCast = response.body.current.weather_descriptions[0];
+    console.log(`${overCast}. It's currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
 })
+
+
+
