0422bbdf26a924b2187107405c70d2b84d80ec01
linlinliu <linlinliu03@gmail.com>
2020-06-30 10:15:44 -0700

    Refactor weather method

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-const request = require("request");
 const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-// const url = "http://api.weatherstack.com/current?access_key=747ab087e2746709e77999472c1c284c&query=New%20York&units=f";
-
-// request({url:url, json:true}, (error,response) => {
-
-//     if(error){
-//         console.log("Unable to connect weather service!")
-//     }else if(response.body.error){
-//         console.log("Unable to find the location!")
-//     }else{
-//         const temperature = response.body.current.temperature;
-//         const feelslike = response.body.current.feelslike;
-//         const overCast = response.body.current.weather_descriptions[0];
-//         console.log(`${overCast}. It's currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
-//     }
-// })
-
-
+forecast(37.8267, -112, (error, data) => {
+    console.log("Error", error);
+    console.log("Data", data);
+})
 
 geocode("New York", (error, data) =>{
     console.log("Error", error);
---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=747ab087e2746709e77999472c1c284c&query=${latitude, longitude}&units=f`;
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback("Unable to connect weather service!", undefined)
+        } else if (response.body.error) {
+            callback("Unable to find the location!", undefined)
+        } else {
+            const temperature = response.body.current.temperature;
+            const feelslike = response.body.current.feelslike;
+            const overCast = response.body.current.weather_descriptions[0];
+            callback(undefined, `${overCast}. It's currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
+        }
+    })
+}
+
+module.exports = forecast;
