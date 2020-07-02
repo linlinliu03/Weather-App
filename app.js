f42dc9b8fd26b4c9085b8ff4f5bc150bf65ed2e0
linlinliu <linlinliu03@gmail.com>
2020-07-02 15:42:54 -0700

    Add location input on the command

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
+const yargs = require("yargs");
 
+const address = process.argv[2];
 
-geocode("New York", (error, data) =>{
-    if(error){
-        return console.log(error)
-    }
-
-    forecast(data.latitude, data.longitude, (error, response) => {
-        if(error){
-            return console.log(error);
+if(!address){
+   console.log("Please type some location to check!")
+}else{
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
         }
-        console.log(data.location)
-        console.log("Data", response);
+
+        forecast(data.latitude, data.longitude, (error, response) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(data.location)
+            console.log(response);
+        })
     })
-})
+}
+
+
+
+
 
 
 
