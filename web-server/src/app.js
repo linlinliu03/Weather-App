a6960684a3155809dda82eaff7cd60951501989b
linlinliu <linlinliu03@gmail.com>
2020-07-16 11:32:01 -0700

    Setup static page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,10 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("", (req,res) => {
-    res.send("Hi express!")
-});
+const publicDirectoryPath = path.join(__dirname,"../public");
 
-
-app.get("/help", (req,res) => {
-    res.send("Help page!")
-});
-
-app.get("/about", (req,res) => {
-    res.send("About page!")
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get("/weather", (req,res) => {
     res.send("Weather page!")
