aabd77d033ae59d3a3f5043fd6a2edfc9ecc5652
linlinliu <linlinliu03@gmail.com>
2020-07-21 11:41:28 -0700

    Inject content to the hbs file

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -9,7 +9,10 @@ app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req,res) => {
-    res.render("index")
+    res.render("index", {
+        title: "Weather App",
+        name: "Linda"
+    })
 })
 
 app.get("/weather", (req,res) => {
